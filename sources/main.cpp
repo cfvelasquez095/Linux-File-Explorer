e89fafd93281bb4edde2f361a167992865cbe470
Carlos Velasquez <fernando@pop-os.localdomain>
2020-09-08 18:35:09 -0600

    Now showing files from root folder on screen.

--- sources/main.cpp
+++ sources/main.cpp
@@ -6,8 +6,55 @@
 #include "../headers/FileSys.h"
 #include <vector>
 
+std::vector<TextView> getTextViewsFiles(std::vector<struct entry> files)
+{
+    std::vector<TextView> textos;
+    int j = 1;
+    int m = 1;
+    int l = 1;
+    for(int i = 0; i < files.size(); i++) {
+        if(i > 12 && i < 36) {
+            textos.push_back(TextView(files[i].name,335,(m+1)*35));    
+            m++;
+        }
+        else if(i > 35) {
+            textos.push_back(TextView(files[i].name,445,(l+1)*30));    
+            l++;
+        }
+        else {
+            textos.push_back(TextView(files[i].name,225,(i+2)*35));
+        }
+    }
+    return textos;
+}
+
+
+std::vector<TextView> createTextViewsPartitions(std::vector<struct Mount> partitions)
+{
+    std::vector<TextView> textos;
+    for(int i = 0; i < partitions.size(); i++) {
+        textos.push_back(TextView(partitions[i].destination,10,(i+1)*40));
+    }
+
+    return textos;
+}
+
+std::string getEntryFocused(std::vector<TextView> entries)
+{
+    for(int i = 0; i < entries.size(); i++) {
+        if(entries[i].isFocused())
+            return entries[i].getText();
+    }
+
+    return "";
+}
+
 int main()
 {
+    FileSys f = FileSys();
+    std::vector<TextView> partitionsNames = createTextViewsPartitions(f.partitions);
+    std::vector<TextView> filesInPath = getTextViewsFiles(f.entriesInDir);
+
     RenderWindow x;
     TextView createFolderBtn = TextView("Create Directory",230,555);
     TextView createArchivoBtn = TextView("Create File",230,585);
@@ -36,7 +83,17 @@ int main()
         x.draw(forwardBtn);
         x.draw(mvBtn);
         x.draw(lnHBtn);
-        x.draw(lnSBtn);  
+        x.draw(lnSBtn);
+
+        for(int i = 0; i < partitionsNames.size(); i++) {
+            x.draw(partitionsNames[i]);
+        }
+
+        for(int i = 0; i < filesInPath.size(); i++) {
+            if(filesInPath[i].isFocused())
+                x.draw(filesInPath[i].sayFocused);
+            x.draw(filesInPath[i]);
+        }
     }
 
     return 0;
