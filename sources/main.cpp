51471586584eb9255dfe259d3766f2474ac2e4d8
Carlos Velasquez <cfvelasquez095@gmail.com>
2020-09-08 19:37:13 -0600

    Further design improvements.

--- sources/main.cpp
+++ sources/main.cpp
@@ -9,16 +9,15 @@
 std::vector<TextView> getTextViewsFiles(std::vector<struct entry> files)
 {
     std::vector<TextView> textos;
-    int j = 1;
     int m = 1;
     int l = 1;
     for(int i = 0; i < files.size(); i++) {
         if(i > 12 && i < 36) {
-            textos.push_back(TextView(files[i].name,335,(m+1)*35));    
+            textos.push_back(TextView(files[i].name,335,(m+1)*35));
             m++;
         }
         else if(i > 35) {
-            textos.push_back(TextView(files[i].name,445,(l+1)*30));    
+            textos.push_back(TextView(files[i].name,445,(l+1)*35));
             l++;
         }
         else {
@@ -33,7 +32,7 @@ std::vector<TextView> createTextViewsPartitions(std::vector<struct Mount> partit
 {
     std::vector<TextView> textos;
     for(int i = 0; i < partitions.size(); i++) {
-        textos.push_back(TextView(partitions[i].destination,10,(i+1)*40));
+        textos.push_back(TextView(partitions[i].destination,20,(i+2)*35));
     }
 
     return textos;
@@ -63,8 +62,8 @@ int main()
     TextView mvBtn = TextView("Move File/Dir",570,555);
     TextView lnHBtn = TextView("Create Hard Link",570,585);
     TextView lnSBtn = TextView("Create Soft Link",740,555);
-    TextView returnBtn = TextView("<-Back",220,25);
-    TextView forwardBtn = TextView("Forward->",280,25);
+    TextView returnBtn = TextView("<-Back",15,23);
+    TextView forwardBtn = TextView("Forward->",75,23);
     x.createWindow(900,600,"Linux File Explorer");
     
 
@@ -73,20 +72,21 @@ int main()
         XEvent e;
         e = x.pollEvent(e);
 
-        x.drawSidebar(Rect(0,0,190,525));
+        x.drawNavbar(Rect(0,0,900,35));
+        x.drawSidebar(Rect(0,36,190,489));
         x.draw(Rect(0,525,900,74));
         x.draw(createFolderBtn);
         x.draw(createArchivoBtn);
         x.draw(deleteBtn);
         x.draw(copyBtn);
-        x.draw(returnBtn);
-        x.draw(forwardBtn);
+        x.drawNavbuttons(returnBtn);
+        x.drawNavbuttons(forwardBtn);
         x.draw(mvBtn);
         x.draw(lnHBtn);
         x.draw(lnSBtn);
 
         for(int i = 0; i < partitionsNames.size(); i++) {
-            x.draw(partitionsNames[i]);
+            x.drawPartitions(partitionsNames[i]);
         }
 
         for(int i = 0; i < filesInPath.size(); i++) {
